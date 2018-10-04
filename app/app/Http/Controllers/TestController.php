f1efba17733b113e34b598538425e2f7a0568205
EVOSandru6 <evosduple@mail.ru>
2018-10-04 11:20:52 +0300

    add Doctrine support

--- app/app/Http/Controllers/TestController.php
+++ app/app/Http/Controllers/TestController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use LaravelDoctrine\ORM\Facades\EntityManager;
+
 class TestController extends Controller
 {
     public function index()
     {
         phpinfo();
+
+        $task = new \App\Entities\Task('Make test app', 'Create the test application for the Sitepoint article.');
+
+        EntityManager::persist($task);
+        EntityManager::flush();
+
+        return 'added!';
     }
 }
